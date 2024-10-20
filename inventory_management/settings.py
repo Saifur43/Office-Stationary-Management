eaefa7cafb999f4350ab05bc3e9bc2b7370be5af
saifur <saifurrahmany43@gmail.com>
2024-10-20 22:43:49 +0600

    vercel

--- inventory_management/settings.py
+++ inventory_management/settings.py
@@ -19,7 +19,6 @@ ALLOWED_HOSTS = [
     'localhost',
     '192.168.7.115',
     '.vercel.app',
-    '.now.sh',
 ]
 
 # Application definition
@@ -116,7 +115,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+# Static files configuration
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
